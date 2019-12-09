c89dd539356434c7a15d06f9f723d31ba4c94801
iyaja <ajayuppili@gmail.com>
2019-12-09 13:22:18 -0600

    test: change server.py

--- app/server.py
+++ app/server.py
@@ -18,6 +18,7 @@ path = Path(__file__).parent
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type', 'Service-Worker-Allowed'])
 app.mount('/static', StaticFiles(directory='app/static'))
+app.mount('/', StaticFiles(directory='app/'))
 
 
 async def download_file(url, dest):
