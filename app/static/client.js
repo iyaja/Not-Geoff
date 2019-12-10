94fa076a8e2f017da9af29f44980951189540748
iyaja <ajayuppili@gmail.com>
2019-12-09 22:49:38 -0600

    debug: add print JSON to console for debugging

--- app/static/client.js
+++ app/static/client.js
@@ -28,6 +28,7 @@ function analyze() {
   };
   xhr.onload = function(e) {
     if (this.readyState === 4) {
+      console.log(e.target.responseText);
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `This person is probably ${response["result"]}`;
     }
