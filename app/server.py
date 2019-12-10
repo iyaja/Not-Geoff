3f070033033e720c3f2488e8e9d766525bbd21b7
iyaja <ajayuppili@gmail.com>
2019-12-10 01:36:01 -0600

    test: update static mount directory for cross platform install

--- app/server.py
+++ app/server.py
@@ -18,7 +18,7 @@ path = Path(__file__).parent
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 # app.mount('/static', StaticFiles(directory='app/static'))
-app.mount('/static', StaticFiles(directory='app/static'))
+app.mount('/', StaticFiles(directory='app/static'))
 # app.mount('/pwabuilder-sw.js', StaticFiles(directory='app/static/pwabuilder-sw.js'))
 
 
